54aa6b7da0a66303e5546d861898b7863f64a94e
root <root@ip-10-0-1-154.us-west-2.compute.internal>
2019-07-15 15:30:40 +0000

    added ssh authorized key

--- code/modules/sshd/site.pp
+++ code/modules/sshd/site.pp
@@ -19,4 +19,9 @@ class sshd {
 		ensure	=> running,
 		subscribe => File["/etc/ssh/sshd_config"],
 	}
+	ssh_authorized_key { "manhimf_key":
+		user   => "ubuntu",
+		ensure => present,
+		key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC7gX4g8t3LJojLJKNPUnfb9hcSo+GJzkzriJsN56Rsft3Xz65WXvJTE22ujP9HLOLhT353MsNfEuOTgIeg1r4V2RRRRngF2DOXlMNo9uTa2RHWw2a4jJGFvfN19rdCaeC5jWPrirc2J4z4zwygRYkcZhSiQPJHzZNZH6tP3qlKrJSBypx8H8cxwcqGMKFWS21Tfhywv/KN5xsu2VuwI71DNcQeEIi7sDkYJ33mKE8NpNUNn3tW1Y1/rlqN21WJJNsqEEFb/ogNtUGPdySgfDyJk3i41QSEadyLp9ea8+wOG5KY2iudhxK4GDUNpRI030hh2Rg0OMESWY3sFF2LHbbL", 
+	}
 }
