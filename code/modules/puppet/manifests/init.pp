e9b472e9b2fd078b93e9793ef3ae3158a333637f
karmstr7 <karmstr7@cs.uoregon.edu>
2019-07-15 14:39:41 -0700

    something puppet

--- code/modules/puppet/manifests/init.pp
+++ code/modules/puppet/manifests/init.pp
@@ -1,6 +1,7 @@
 # update /etc/puppet and run "puppet apply":
 class puppet {
 	cron { "puppet apply":
+		ensure 	=> present,
 		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
 		user    => root,
 		minute  => "*/60",
--- manifests/site.pp
+++ manifests/site.pp
@@ -7,14 +7,14 @@ node ip-10-0-1-154 {
 	include sshd
 }
 
-# # update /etc/puppet and run "puppet apply":
-# class puppet {
-# 	cron { "puppet apply":
-# 		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
-# 		user    => root,
-# 		minute  => "*/5",
-# 	}
-# }
+# update /etc/puppet and run "puppet apply":
+class puppet {
+	cron { "puppet apply":
+		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
+		user    => root,
+		minute  => "*/5",
+	}
+}
 
 # class sshd {
 # 	package {
