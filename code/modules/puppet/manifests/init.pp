9defe5cbfba4d737668d9a90675d2c39848f74f7
karmstr7 <karmstr7@cs.uoregon.edu>
2019-07-15 14:36:16 -0700

    more stuff

--- code/modules/puppet/manifests/init.pp
+++ code/modules/puppet/manifests/init.pp
@@ -3,6 +3,6 @@ class puppet {
 	cron { "puppet apply":
 		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
 		user    => root,
-		minute  => "*/5",
+		minute  => "*/60",
 	}
 }
