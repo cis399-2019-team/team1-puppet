7b2a696d048792ba6999f04636b949001e694f85
root <root@ip-10-0-1-154.us-west-2.compute.internal>
2019-07-15 14:27:16 +0000

    added sshd module

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,10 @@
 # update /etc/puppet and run "puppet apply":
 node ip-10-0-1-84 {
-  cron {"puppet apply":
-    command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
-    user => root,
-    minute => "*/5",
-  }
+	 include puppet 
+	include sshd
 }
 
 node ip-10-0-1-154 {
-  cron {"puppet apply":
-    command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
-    user => root,
-    minute => "*/5",
-  }
+	include puppet
+	incldue sshd
 }
