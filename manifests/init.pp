09608f8c2e5e61738ea3c2c4709c8a2bd3482e60
karmstr7 <karmstr7@cs.uoregon.edu>
2019-07-15 13:43:19 -0700

    separated puppet class from site.pp

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+# update /etc/puppet and run "puppet apply":
+class puppet {
+	cron { "puppet apply":
+		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
+		user    => root,
+		minute  => "*/5",
+	}
+}
--- manifests/site.pp
+++ manifests/site.pp
@@ -6,14 +6,3 @@ node ip-10-0-1-154 {
 	include puppet
 	include sshd
 }
-
-# update /etc/puppet and run "puppet apply":
-class puppet {
-	cron { "puppet apply":
-		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
-		user    => root,
-		minute  => "*/5",
-	}
-}
-
-
