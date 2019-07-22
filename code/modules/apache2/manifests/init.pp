1a1ca5f28a991686660d339e34da719681aeff7f
karmstr7 <karmstr7@cs.uoregon.edu>
2019-07-22 13:21:12 -0700

    small changes in code

--- code/modules/apache2/manifests/init.pp
+++ code/modules/apache2/manifests/init.pp
@@ -4,20 +4,20 @@ class apache2 {
 		provider	=> apt,
 	}
 
-	file { "/etc/apache2/apache2_config":
+	file { "/etc/apache2/apache2.conf":
 		ensure  => present,
-		notify 	=> Service["apache2"]
+		notify 	=> Service["apache2"],
 		mode    => '444',
 		owner   => 'root',
 		group   => 'root',
-		source  => "puppet:///modules/apache2/apache2_config",
+		source  => "puppet:///modules/apache2/apache2.conf",
 		require => Package["apache2"],
 	}
 
 	service { "apache2":
 		enable    => true,
 		ensure    => running,
-		subscribe => File["/etc/apache2/apache2_config"],
+		subscribe => File["/etc/apache2/apache2.config"],
 		require   => [
 						Package["apache2"],
 		],
