e5cfac596bdc3203f8908b8b6134ac49b3d0f068
karmstr7 <karmstr7@cs.uoregon.edu>
2019-07-22 19:06:22 -0700

    apache2 module change

--- code/modules/apache2/manifests/init.pp
+++ code/modules/apache2/manifests/init.pp
@@ -32,14 +32,4 @@ class apache2 {
 		source => "puppet:///modules/apache2/html",
 		require => Package["apache2"],
 	}
-
-	# file { "/var/www/html/foo.html":
-	# 	ensure => directory,
-	# 	recurse => true,
-	# 	mode => '0644',
-	# 	owner => 'root',
-	# 	group => 'root',
-	# 	source => "puppet:///modules/apache2/html/foo.html",
-	# 	require => Package["apache2"],
-	# }
 }
