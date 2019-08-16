01730d6edff3473995c2e833413ad7c7c2acea62
keir <karmstr7@uoregon.edu>
2019-08-16 14:46:09 -0700

    new modules

--- code/modules/apache2/files/html/index.php
+++ code/modules/apache2/files/html/index.php
@@ -1,8 +1,12 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>This is a PHP file!</title>
  </head>
  <body>
- <?php echo '<p>Hello World<99/p>'; ?> 
+ <?php echo '<p>Hello World</p>'; 
+ $usersFile = fopen("/userslist.txt", "r") or die ("Unable to open file1!");
+ echo fgets($usersFile);
+ fclose($usersFile);
+ ?> 
  </body>
 </html>
