b7ff7e30ba828b3f306260cac1a7c7ff06e4cead
keir <karmstr7@uoregon.edu>
2019-08-16 16:09:14 -0700

    new modules

--- code/modules/apache2/files/html/index.php
+++ code/modules/apache2/files/html/index.php
@@ -2,29 +2,26 @@
  <head>
   <title>This is a PHP file!</title>
  </head>
+ <meta http-equiv="refresh" content="60">
  <body>
  <?php echo '<p>Hello World</p>'; 
  echo '<p>Registered Users</p>';
- set_time_limit(0);
- while(true) {
-    $usersFile = fopen(__DIR__ . '/userslist.txt', 'r') or die ("Unable to open file1!");
-    while(!feof($usersFile)) {
-        echo fgets($usersFile) . "<br>";
-    }
-    fclose($usersFile);
-    sleep(30);
- }
+
+$usersFile = fopen(__DIR__ . '/userslist.txt', 'r') or die ("Unable to open file1!");
+while(!feof($usersFile)) {
+    echo fgets($usersFile) . "<br>";
+}
+fclose($usersFile);
+sleep(30);
 
 
  echo '<p>Recently authenticated users</p>';
- while(true) {
-    $lastUsersFile = fopen(__DIR__ .'/userslast.txt', 'r') or die ("Unable to open file2");
-    while(!feof($lastUsersFile)) {
-        echo fgets($lastUsersFile) . "<br>";
-    }
-    fclose($lastUsersFile);
-    sleep(30);
- }
+$lastUsersFile = fopen(__DIR__ .'/userslast.txt', 'r') or die ("Unable to open file2");
+while(!feof($lastUsersFile)) {
+    echo fgets($lastUsersFile) . "<br>";
+}
+fclose($lastUsersFile);
+sleep(30);
  ?> 
  </body>
 </html>
