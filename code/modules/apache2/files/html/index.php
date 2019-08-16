407cf21e241465eaaff523ddd875f99e70fe5e2f
keir <karmstr7@uoregon.edu>
2019-08-16 15:47:48 -0700

    new modules

--- code/modules/apache2/files/html/index.php
+++ code/modules/apache2/files/html/index.php
@@ -5,18 +5,18 @@
  <body>
  <?php echo '<p>Hello World</p>'; 
  echo '<p>Registered Users</p>';
- $usersFile = fopen(__DIR__ . '/userslist.txt', 'r') or die ("Unable to open file1!");
+ $usersFile = fopen(__DIR__ . '/commandsList.txt', 'r') or die ("Unable to open file1!");
  while(!feof($usersFile)) {
      echo fgets($usersFile) . "<br>";
  }
  fclose($usersFile);
 
  echo '<p>Recently authenticated users</p>';
- $lastUsersFile = fopen(__DIR__ .'/userslast.txt', 'r') or die ("Unable to open file2");
- while(!feof($lastUsersFile)) {
-     echo fgets($lastUsersFile) . "<br>";
- }
- fclose($lastUsersFile);
+//  $lastUsersFile = fopen(__DIR__ .'/userslast.txt', 'r') or die ("Unable to open file2");
+//  while(!feof($lastUsersFile)) {
+//      echo fgets($lastUsersFile) . "<br>";
+//  }
+//  fclose($lastUsersFile);
  ?> 
  </body>
 </html>
